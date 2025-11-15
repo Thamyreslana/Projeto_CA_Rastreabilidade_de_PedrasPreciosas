d7e78a9311887bec8cb95bcff92e741d7d987869
THAMYRES DE LANA THIMOTEO LEAL E SILVA <thamyreslana@uni9.edu.br>
2025-11-15 16:07:05 -0300

    main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,24 +1,11 @@
-async function carregarPedras() {
-  cons resposta = await fetch("assets/data/pedras.json");
-  cons pedras = await resposta.json();
-  cons lista = document.getElemenById("lista-pedras");
-  
-  pedras.forEach(pedra => {
-    const div = document.createElement("div");
-    div.classList.add("pedra-card");
-    div.innerHTML = `
-      <h2>${pedra.tipo} (${pedra.cor})</h2>
-      <p><strong>Peso:</strong> ${pedra.peso}g</p>
-      <p><strong>Categoria:</strong> ${pedra.categoria}</p>
-      <p><storng>Local:</strong> ${pedra.local_extracao}</p>
+funtion mostrarPedras(){
+  const lista = document.getElementById("lista-pedras");
+  lista.innerHTML = "";
+} 
 
-      <h3>Garimpeiro</h3>
-      <p>${pedra.garimpeiro.nome}</p>
-
-      <h3>Método de Garimpo</h3>
-      <p>${pedra.metoso_garimpo.tecnica}</p>
-  ´;
-   lista.appendChild(div);
-   });
+  const pedras = JSON.parse(localStorage.getItem("pedras")) || [];
+  pedras.forEach(p => {
+    lista.innerHTML += `<p>${p.tipo} - ${p.cor}</p>`;
+  });
 }
-carregarPedras();
+mostrarPedras();
